3ae5a4c6fee52e05920c701b1423cf2f17251af4
Joaomgsa <joaomgsa@gmail.com>
2023-02-12 20:01:57 -0300

    iniciando a manipulação do dom

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -1,5 +1,24 @@
-console.log(window.location.href);
+const item1 = document.querySelector('li');
+console.log(item1);
+console.log(item1.innerHTML);
+console.log(item1.outerHTML);
 
-console.log(document);
+//colocando todos os itens LI dentro de uma nodelist
+const items = document.querySelectorAll('li');
+console.log(items);
 
-window.alert("Olá mundo");
+const card1 = document.querySelector('.card');
+
+console.log(card1);
+
+const paragrafo = document.createElement('p');
+paragrafo.innerText = "Parágrafo inserido via JS";
+card1.appendChild(paragrafo);
+card1.classList.add('super-border');
+
+const cards = document.querySelectorAll('.card');
+console.log(cards);
+
+Array.from(cards).forEach(card => {
+    console.log(card.innerHTML);
+});
